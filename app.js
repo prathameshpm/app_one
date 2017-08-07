7661ec5377c31cb5c029af398bc01cab7dadc5c0
prathameshpm <mahamulkarprathamesh@gmail.com>
2017-08-07 20:13:01 +0530

    Part 1, ToDo 2 : Get Input Values

--- app.js
+++ app.js
@@ -5,16 +5,41 @@ var budgetController = (function () {
 
 // UI Controller
 var UIController = (function () {
+    
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+    
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
 
 })();
 
 // Global App Controller
 var controller = (function (budgetCtrl, UICtrl) {
-
+    
+    var DOM = UICtrl.getDOMstrings();
+    
     var ctrlAddItem = function () {
 
         // 1. Get the field input data
-
+        var input = UICtrl.getInput();
+        
+        
         // 2. Add the item to the budget controller
 
         // 3. Add the item to the UI
@@ -27,7 +52,7 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     };
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function (event) {
 
